b134955ed4c0e6f4539d4c540a1ed43f78425409
V0idMatr1x <v0idmatr1x.0@gmail.com>
2024-03-20 14:50:01 -0500

    Update: chalk color

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import ollama
 from loguru import logger
-from simple_chalk import chalk, green
+from simple_chalk import chalk, red
 
-name = input(chalk.green.bold("Choose a name for your model: "))
-model_selection = input(chalk.green.bold("Select a model to train: "))
-prompt = input(chalk.green.bold("Enter a prompt to train the model with: "))
+name = input(chalk.red.bold("Choose a name for your model: "))
+model_selection = input(chalk.red.bold("Select a model to train: "))
+prompt = input(chalk.red.bold("Enter a prompt to train the model with: "))
   
 
 modelfile = f"""
